048b466b1f6e0c4cf74d3e7bdfc2d0abfdab2465
julia <juhmanayra@gmail.com>
2021-10-06 20:15:32 -0300

    melhorando performace

--- Pratice Test Visual Nuts/test-visual-nutes/src/com/visual/nutes/julia/main/Main.java
+++ Pratice Test Visual Nuts/test-visual-nutes/src/com/visual/nutes/julia/main/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         var nutesUtil = new NumberVisualNutesUtil();
         List<String> list = nutesUtil.getListNumberReplaceToVisualNutesDivisibleByTwoNumberOrIndividualNumber(100,3,5);
-        list.stream().forEach(System.out::println);
+        list.forEach(System.out::println);
     }
 }
